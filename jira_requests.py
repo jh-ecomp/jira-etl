6f1b9d8d3f72a2e58e8988ce1d9c27fad4319bb4
Joao_Henrique_Trixlog <joaohenrique@trixlog.com>
2021-09-17 21:07:01 -0300

    cleaning jira.config file and create main jira request

--- jira_requests.py
+++ jira_requests.py
@@ -1,4 +1,5 @@
-from requests import Request, Session
+import requests
+from requests import Session
 from requests.auth import HTTPBasicAuth
 from requests.adapters import HTTPAdapter
 from requests.packages.urllib3.util.retry import Retry
@@ -19,7 +20,7 @@ def mount_jira_session():
     session = Session()
     session.mount('http://', HTTPAdapter(max_retries=retries))
     response = None
-    
+
     try:
         response = session.get(url, auth=auth)
     except Timeout as t:
@@ -30,4 +31,30 @@ def mount_jira_session():
         traceback.print_exc()
 
     return response
+
+
+def get_jira_main_request(main_request, jql, fields, expand, start_at, max_results, cookies):
+
+    url = f'{main_request}?jql={jql}&fields={fields}&expand={expand}&startAt={start_at}&maxResults={max_results}'
+    response = None
+    try:
+        response = requests.get(url=url, cookies=cookies, timeout=60)
+    except Timeout as t:
+        print(f'Jira request timeout {t}')
+        traceback.print_exc()
+    except Exception as e:
+        print(f'Jira request exception {e}')
+        traceback.print_exc()
+
+    if response is None: return response
+
+    try:
+        json = response.json()
+        issues = json['issues']
+    except Exception as e:
+        print(f'Failed to load response content')
+        issues = None
+
+
+    return issues
 
